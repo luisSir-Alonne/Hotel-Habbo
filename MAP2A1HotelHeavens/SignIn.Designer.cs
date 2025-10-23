dd46090b0cbcc7ca92d61766254009cf7d4b99be
Luis Daniel Balderas Muñoz <luis.danielbalmu@gmail.com>
2025-10-23 12:24:34 -0600

    Relize el registro de usuario

--- MAP2A1HotelHeavens/SignIn.Designer.cs
+++ MAP2A1HotelHeavens/SignIn.Designer.cs
@@ -28,11 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblMenuCerrar = new System.Windows.Forms.Label();
             this.picMinimizar = new System.Windows.Forms.PictureBox();
             this.picCerrar = new System.Windows.Forms.PictureBox();
+            this.chkMostrarPass = new System.Windows.Forms.CheckBox();
+            this.mtbTelefono = new System.Windows.Forms.MaskedTextBox();
+            this.mtbPass2 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbPass = new System.Windows.Forms.MaskedTextBox();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.chkTyC = new System.Windows.Forms.CheckBox();
+            this.txtCorreo2 = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblConfirmarPass = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblConfirmarCorreo = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.lblMensaje = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picMinimizar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblMenuCerrar
@@ -41,14 +62,14 @@
             this.lblMenuCerrar.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblMenuCerrar.Location = new System.Drawing.Point(0, 0);
             this.lblMenuCerrar.Name = "lblMenuCerrar";
-            this.lblMenuCerrar.Size = new System.Drawing.Size(379, 43);
+            this.lblMenuCerrar.Size = new System.Drawing.Size(873, 43);
             this.lblMenuCerrar.TabIndex = 6;
             // 
             // picMinimizar
             // 
             this.picMinimizar.BackColor = System.Drawing.Color.Peru;
             this.picMinimizar.Image = global::MAP2A1HotelHeavens.Properties.Resources.menos;
-            this.picMinimizar.Location = new System.Drawing.Point(305, 0);
+            this.picMinimizar.Location = new System.Drawing.Point(577, 0);
             this.picMinimizar.Name = "picMinimizar";
             this.picMinimizar.Size = new System.Drawing.Size(34, 43);
             this.picMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -59,18 +80,245 @@
             // 
             this.picCerrar.BackColor = System.Drawing.Color.Peru;
             this.picCerrar.Image = global::MAP2A1HotelHeavens.Properties.Resources.cerrar;
-            this.picCerrar.Location = new System.Drawing.Point(345, 0);
+            this.picCerrar.Location = new System.Drawing.Point(617, 0);
             this.picCerrar.Name = "picCerrar";
             this.picCerrar.Size = new System.Drawing.Size(34, 43);
             this.picCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picCerrar.TabIndex = 7;
             this.picCerrar.TabStop = false;
             // 
+            // chkMostrarPass
+            // 
+            this.chkMostrarPass.AutoSize = true;
+            this.chkMostrarPass.Font = new System.Drawing.Font("Elephant", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkMostrarPass.ForeColor = System.Drawing.Color.DarkOrange;
+            this.chkMostrarPass.Location = new System.Drawing.Point(15, 301);
+            this.chkMostrarPass.Margin = new System.Windows.Forms.Padding(2);
+            this.chkMostrarPass.Name = "chkMostrarPass";
+            this.chkMostrarPass.Size = new System.Drawing.Size(172, 22);
+            this.chkMostrarPass.TabIndex = 39;
+            this.chkMostrarPass.Text = "Mostrar contraseñas";
+            this.chkMostrarPass.UseVisualStyleBackColor = true;
+            // 
+            // mtbTelefono
+            // 
+            this.mtbTelefono.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbTelefono.Location = new System.Drawing.Point(99, 270);
+            this.mtbTelefono.Margin = new System.Windows.Forms.Padding(2);
+            this.mtbTelefono.Mask = "000-000-0000";
+            this.mtbTelefono.Name = "mtbTelefono";
+            this.mtbTelefono.Size = new System.Drawing.Size(101, 28);
+            this.mtbTelefono.TabIndex = 38;
+            // 
+            // mtbPass2
+            // 
+            this.mtbPass2.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbPass2.Location = new System.Drawing.Point(195, 238);
+            this.mtbPass2.Margin = new System.Windows.Forms.Padding(2);
+            this.mtbPass2.Name = "mtbPass2";
+            this.mtbPass2.Size = new System.Drawing.Size(248, 28);
+            this.mtbPass2.TabIndex = 37;
+            this.mtbPass2.UseSystemPasswordChar = true;
+            // 
+            // mtbPass
+            // 
+            this.mtbPass.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbPass.Location = new System.Drawing.Point(118, 202);
+            this.mtbPass.Margin = new System.Windows.Forms.Padding(2);
+            this.mtbPass.Name = "mtbPass";
+            this.mtbPass.Size = new System.Drawing.Size(243, 28);
+            this.mtbPass.TabIndex = 36;
+            this.mtbPass.UseSystemPasswordChar = true;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.Wheat;
+            this.btnRegresar.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnRegresar.Location = new System.Drawing.Point(143, 355);
+            this.btnRegresar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(99, 33);
+            this.btnRegresar.TabIndex = 35;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.Wheat;
+            this.btnRegistrar.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnRegistrar.Location = new System.Drawing.Point(15, 355);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(110, 33);
+            this.btnRegistrar.TabIndex = 34;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // chkTyC
+            // 
+            this.chkTyC.AutoSize = true;
+            this.chkTyC.Font = new System.Drawing.Font("Elephant", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkTyC.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.chkTyC.Location = new System.Drawing.Point(15, 327);
+            this.chkTyC.Margin = new System.Windows.Forms.Padding(2);
+            this.chkTyC.Name = "chkTyC";
+            this.chkTyC.Size = new System.Drawing.Size(279, 24);
+            this.chkTyC.TabIndex = 33;
+            this.chkTyC.Text = "Acepto los términos y condiciones";
+            this.chkTyC.UseVisualStyleBackColor = true;
+            // 
+            // txtCorreo2
+            // 
+            this.txtCorreo2.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCorreo2.Location = new System.Drawing.Point(238, 162);
+            this.txtCorreo2.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCorreo2.Name = "txtCorreo2";
+            this.txtCorreo2.Size = new System.Drawing.Size(392, 28);
+            this.txtCorreo2.TabIndex = 32;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(170, 99);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(437, 28);
+            this.txtNombre.TabIndex = 31;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCorreo.Location = new System.Drawing.Point(170, 131);
+            this.txtCorreo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(437, 28);
+            this.txtCorreo.TabIndex = 30;
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefono.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblTelefono.Location = new System.Drawing.Point(16, 274);
+            this.lblTelefono.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(83, 21);
+            this.lblTelefono.TabIndex = 29;
+            this.lblTelefono.Text = "Teléfono:";
+            // 
+            // lblConfirmarPass
+            // 
+            this.lblConfirmarPass.AutoSize = true;
+            this.lblConfirmarPass.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmarPass.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblConfirmarPass.Location = new System.Drawing.Point(11, 240);
+            this.lblConfirmarPass.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConfirmarPass.Name = "lblConfirmarPass";
+            this.lblConfirmarPass.Size = new System.Drawing.Size(188, 21);
+            this.lblConfirmarPass.TabIndex = 28;
+            this.lblConfirmarPass.Text = "Confirmar contraseña:";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblPass.Location = new System.Drawing.Point(13, 206);
+            this.lblPass.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(105, 21);
+            this.lblPass.TabIndex = 27;
+            this.lblPass.Text = "Contraseña:";
+            // 
+            // lblConfirmarCorreo
+            // 
+            this.lblConfirmarCorreo.AutoSize = true;
+            this.lblConfirmarCorreo.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmarCorreo.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblConfirmarCorreo.Location = new System.Drawing.Point(11, 168);
+            this.lblConfirmarCorreo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConfirmarCorreo.Name = "lblConfirmarCorreo";
+            this.lblConfirmarCorreo.Size = new System.Drawing.Size(243, 21);
+            this.lblConfirmarCorreo.TabIndex = 26;
+            this.lblConfirmarCorreo.Text = "Confirmar correo electrónico:";
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorreo.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblCorreo.Location = new System.Drawing.Point(13, 133);
+            this.lblCorreo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(160, 21);
+            this.lblCorreo.TabIndex = 25;
+            this.lblCorreo.Text = "Correo electrónico:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblNombre.Location = new System.Drawing.Point(16, 101);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(154, 21);
+            this.lblNombre.TabIndex = 24;
+            this.lblNombre.Text = "Nombre completo:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Elephant", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblTitulo.Location = new System.Drawing.Point(159, 45);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(327, 41);
+            this.lblTitulo.TabIndex = 23;
+            this.lblTitulo.Text = "Registro de usuario";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensaje.ForeColor = System.Drawing.Color.Red;
+            this.lblMensaje.Location = new System.Drawing.Point(16, 390);
+            this.lblMensaje.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(0, 21);
+            this.lblMensaje.TabIndex = 40;
+            // 
             // SignIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 398);
+            this.ClientSize = new System.Drawing.Size(873, 527);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.chkMostrarPass);
+            this.Controls.Add(this.mtbTelefono);
+            this.Controls.Add(this.mtbPass2);
+            this.Controls.Add(this.mtbPass);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.chkTyC);
+            this.Controls.Add(this.txtCorreo2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblConfirmarPass);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblConfirmarCorreo);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.picMinimizar);
             this.Controls.Add(this.picCerrar);
             this.Controls.Add(this.lblMenuCerrar);
@@ -79,7 +327,9 @@
             this.Text = "SignIn";
             ((System.ComponentModel.ISupportInitialize)(this.picMinimizar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -88,5 +338,24 @@
         private System.Windows.Forms.PictureBox picMinimizar;
         private System.Windows.Forms.PictureBox picCerrar;
         private System.Windows.Forms.Label lblMenuCerrar;
+        private System.Windows.Forms.CheckBox chkMostrarPass;
+        private System.Windows.Forms.MaskedTextBox mtbTelefono;
+        private System.Windows.Forms.MaskedTextBox mtbPass2;
+        private System.Windows.Forms.MaskedTextBox mtbPass;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.CheckBox chkTyC;
+        private System.Windows.Forms.TextBox txtCorreo2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblConfirmarPass;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label lblConfirmarCorreo;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label lblMensaje;
     }
 }
--- MAP2A1HotelHeavens/SignIn.cs
+++ MAP2A1HotelHeavens/SignIn.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,97 @@ namespace MAP2A1HotelHeavens
         public SignIn()
         {
             InitializeComponent();
+            chkTyC.CheckedChanged += (s, e) => { };
+            chkMostrarPass.CheckedChanged += chkMostrarPass_CheckedChanged;
+            btnRegistrar.Click += btnRegistrar_Click;
+            btnRegresar.Click += (s, e) => this.Close();
+
+            errorProvider1.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+            mtbPass.UseSystemPasswordChar = true;
+            mtbPass2.UseSystemPasswordChar = true;
+            lblMensaje.Text = "";
+        }
+        private void chkMostrarPass_CheckedChanged(object sender, EventArgs e)
+        {
+            bool mostrar = chkMostrarPass.Checked;
+            mtbPass.UseSystemPasswordChar = !mostrar;
+            mtbPass2.UseSystemPasswordChar = !mostrar;
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            lblMensaje.Text = "";
+            errorProvider1.Clear();
+
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            { Fail(txtNombre, "Escribe tu nombre completo."); return; }
+
+            if (string.IsNullOrWhiteSpace(txtCorreo.Text))
+            { Fail(txtCorreo, "Escribe tu correo."); return; }
+
+            if (!EsEmailValido(txtCorreo.Text))
+            { Fail(txtCorreo, "Correo no válido."); return; }
+
+            if (string.IsNullOrWhiteSpace(txtCorreo2.Text))
+            { Fail(txtCorreo2, "Confirma tu correo."); return; }
+
+            if (!txtCorreo.Text.Equals(txtCorreo2.Text, StringComparison.OrdinalIgnoreCase))
+            { Fail(txtCorreo2, "Los correos no coinciden."); return; }
+
+            if (!mtbTelefono.MaskFull)
+            { Fail(mtbTelefono, "Completa el teléfono (10 dígitos)."); return; }
+
+            var passMsg = ValidaPassword(mtbPass.Text);
+            if (passMsg != null)
+            { Fail(mtbPass, passMsg); return; }
+
+            if (string.IsNullOrEmpty(mtbPass2.Text))
+            { Fail(mtbPass2, "Confirma tu contraseña."); return; }
+
+            if (mtbPass.Text != mtbPass2.Text)
+            { Fail(mtbPass2, "Las contraseñas no coinciden."); return; }
+
+
+            if (!chkTyC.Checked)
+            { Fail(chkTyC, "Debes aceptar los Términos y Condiciones."); return; }
+
+            MessageBox.Show($"¡Registro exitoso!\n\nBienvenido/a, {txtNombre.Text}.",
+                            "Aerolínea", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+
+        private void Fail(Control ctrl, string msg)
+        {
+            errorProvider1.SetError(ctrl, msg);
+            lblMensaje.Text = "❌ " + msg;
+            ctrl.Focus();
+        }
+
+        private bool EsEmailValido(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email)) return false;
+            const string pat = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pat, RegexOptions.IgnoreCase);
+        }
+
+        private string ValidaPassword(string p)
+        {
+            if (string.IsNullOrEmpty(p)) return "Escribe una contraseña.";
+            if (p.Length < 8) return "La contraseña debe tener al menos 8 caracteres.";
+            bool may = false, min = false, dig = false;
+            foreach (char c in p)
+            {
+                if (char.IsUpper(c)) may = true;
+                else if (char.IsLower(c)) min = true;
+                else if (char.IsDigit(c)) dig = true;
+            }
+            if (!may || !min || !dig)
+                return "Incluye mayúsculas, minúsculas y números.";
+            return null; 
         }
     }
 }
