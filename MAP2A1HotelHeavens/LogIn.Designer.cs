afe9c8d1e1588040e1c6a9359b59d0eb4973a2f5
leonelmisael36-ux <leonelmisael36@gmail.com>
2025-10-28 18:39:19 -0600

    Añadir elementos al form Login(diseño).

--- MAP2A1HotelHeavens/LogIn.Designer.cs
+++ MAP2A1HotelHeavens/LogIn.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblMenuCerrar = new System.Windows.Forms.Label();
             this.picMinimizar = new System.Windows.Forms.PictureBox();
             this.picCerrar = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxContraseña = new System.Windows.Forms.TextBox();
+            this.buttonIniciar = new System.Windows.Forms.Button();
+            this.checkBoxOcultar1 = new System.Windows.Forms.CheckBox();
+            this.checkBoxOcultar2 = new System.Windows.Forms.CheckBox();
+            this.notifyIconMenu = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolTipInfoLogin = new System.Windows.Forms.ToolTip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picMinimizar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCerrar)).BeginInit();
             this.SuspendLayout();
@@ -66,12 +76,95 @@
             this.picCerrar.TabIndex = 4;
             this.picCerrar.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(142, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(125, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Contraseña:";
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsuario.Location = new System.Drawing.Point(81, 119);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.PasswordChar = '*';
+            this.textBoxUsuario.Size = new System.Drawing.Size(197, 22);
+            this.textBoxUsuario.TabIndex = 8;
+            // 
+            // textBoxContraseña
+            // 
+            this.textBoxContraseña.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxContraseña.Location = new System.Drawing.Point(81, 190);
+            this.textBoxContraseña.Name = "textBoxContraseña";
+            this.textBoxContraseña.PasswordChar = '*';
+            this.textBoxContraseña.Size = new System.Drawing.Size(197, 22);
+            this.textBoxContraseña.TabIndex = 9;
+            // 
+            // buttonIniciar
+            // 
+            this.buttonIniciar.BackColor = System.Drawing.Color.Peru;
+            this.buttonIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonIniciar.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIniciar.Location = new System.Drawing.Point(107, 240);
+            this.buttonIniciar.Name = "buttonIniciar";
+            this.buttonIniciar.Size = new System.Drawing.Size(153, 41);
+            this.buttonIniciar.TabIndex = 10;
+            this.buttonIniciar.Text = "Iniciar Sesion";
+            this.buttonIniciar.UseVisualStyleBackColor = false;
+            // 
+            // checkBoxOcultar1
+            // 
+            this.checkBoxOcultar1.AutoSize = true;
+            this.checkBoxOcultar1.Location = new System.Drawing.Point(294, 123);
+            this.checkBoxOcultar1.Name = "checkBoxOcultar1";
+            this.checkBoxOcultar1.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxOcultar1.TabIndex = 11;
+            this.checkBoxOcultar1.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxOcultar2
+            // 
+            this.checkBoxOcultar2.AutoSize = true;
+            this.checkBoxOcultar2.Location = new System.Drawing.Point(294, 194);
+            this.checkBoxOcultar2.Name = "checkBoxOcultar2";
+            this.checkBoxOcultar2.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxOcultar2.TabIndex = 12;
+            this.checkBoxOcultar2.UseVisualStyleBackColor = true;
+            // 
+            // notifyIconMenu
+            // 
+            this.notifyIconMenu.Visible = true;
+            // 
+            // toolTipInfoLogin
+            // 
+            this.toolTipInfoLogin.ToolTipTitle = "Login";
+            // 
             // LogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(379, 398);
             this.ControlBox = false;
+            this.Controls.Add(this.checkBoxOcultar2);
+            this.Controls.Add(this.checkBoxOcultar1);
+            this.Controls.Add(this.buttonIniciar);
+            this.Controls.Add(this.textBoxContraseña);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.picMinimizar);
             this.Controls.Add(this.picCerrar);
             this.Controls.Add(this.lblMenuCerrar);
@@ -82,6 +175,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.picMinimizar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCerrar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -90,5 +184,14 @@
         private System.Windows.Forms.PictureBox picMinimizar;
         private System.Windows.Forms.PictureBox picCerrar;
         private System.Windows.Forms.Label lblMenuCerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textBoxContraseña;
+        private System.Windows.Forms.Button buttonIniciar;
+        private System.Windows.Forms.CheckBox checkBoxOcultar1;
+        private System.Windows.Forms.CheckBox checkBoxOcultar2;
+        private System.Windows.Forms.NotifyIcon notifyIconMenu;
+        private System.Windows.Forms.ToolTip toolTipInfoLogin;
     }
 }
