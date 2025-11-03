2ad86a8fcd643890d8f683abd0d082f50e9cf2d1
leonelmisael36-ux <leonelmisael36@gmail.com>
2025-11-02 21:02:09 -0600

    Agregar fondo chekin

--- MAP2A1HotelHeavens/CheckIn.Designer.cs
+++ MAP2A1HotelHeavens/CheckIn.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CheckIn));
             this.lblMenuCerrar = new System.Windows.Forms.Label();
             this.picMinimizar = new System.Windows.Forms.PictureBox();
             this.picCerrar = new System.Windows.Forms.PictureBox();
@@ -394,7 +393,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImage = global::MAP2A1HotelHeavens.Properties.Resources.imgcheckinLP;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1386, 788);
             this.ControlBox = false;
