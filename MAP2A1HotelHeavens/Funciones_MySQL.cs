befb7c08d26eef67778529b472b18e684defce6b
leonel36-x <leonelmisael36@gmail.com>
2025-11-05 11:16:41 -0600

    eliminar otra funcion

--- MAP2A1HotelHeavens/Funciones_MySQL.cs
+++ MAP2A1HotelHeavens/Funciones_MySQL.cs
@@ -9,23 +9,6 @@ namespace MAP2A1HotelHeavens
 {
     public class Funciones_MySQL
     {
-        private string Conexion = "Server=127.0.0.1;Database=hotel;User ID=root;Password=;";
-        
-        
-        public MySqlConnection obtenerconexion()
-        {
-            MySqlConnection con = new MySqlConnection(Conexion);
-
-            try
-            {
-                con.Open();
-                Console.WriteLine("La conexion fue exitosa");
-            }
-            catch(Exception no)
-            {
-                Console.WriteLine("Fallo la conexion" + no.Message);
-            }
-            return con;
-        }
+       
     }
 }
