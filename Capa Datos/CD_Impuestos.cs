76d53d44adbddd59594a9a994310aa96187ee0ca
Luis Ponce <luisponceprieto@gmail.com>
2025-11-10 22:30:35 -0600

    Octavo commit para las clases de impuestos en cada una de sus capas

--- Capa Datos/CD_Impuestos.cs
+++ Capa Datos/CD_Impuestos.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Capa_Entidad;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,39 @@ namespace Capa_Datos
 {
     public class CD_Impuestos
     {
+        public List<Impuestos> Leer()
+        {
+            List<Impuestos> impuestos = new List<Impuestos>();
+            using (MySqlConnection oconexion = new MySqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    string query = "SELECT * FROM impuestos";
+                    MySqlCommand cmd = new MySqlCommand(query.ToString(), oconexion);
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    oconexion.Open();
+                    using (MySqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            impuestos.Add(new Impuestos()
+                            {
+                               idImpuesto = Convert.ToInt32(dr["idImpuesto"].ToString()),
+                               impuesto = dr["impuesto"].ToString(),
+                               costo = Convert.ToDecimal(dr["costo"].ToString())
+                            });
+                        }
+                    }
+                }
+                catch
+                {
+                    impuestos = new List<Impuestos>();
+                }
+            }
+
+            return impuestos;
+        }
+
+       
     }
 }
--- Capa Negocio/CN_Impuestos.cs
+++ Capa Negocio/CN_Impuestos.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Capa_Datos;
+using Capa_Entidad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace Capa_Negocio
 {
     public class CN_Impuestos
     {
+        private CD_Impuestos obj_impuestos = new CD_Impuestos();
+        public List<Impuestos> Leer()
+        {
+            return obj_impuestos.Leer();
+        }
     }
 }
