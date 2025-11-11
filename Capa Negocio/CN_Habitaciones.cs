174010281c1af67c316e97649015293d8ec4b534
Luis Ponce <luisponceprieto@gmail.com>
2025-11-10 22:06:44 -0600

    Sexto commit, implementacion de la tabla de habitaciones en las tres capas

--- Capa Negocio/CN_Habitaciones.cs
+++ Capa Negocio/CN_Habitaciones.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capa_Entidad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Capa_Negocio
 {
     public class CN_Habitaciones
     {
+        private CN_Habitaciones obj_habitaciones = new CN_Habitaciones();
+        public List<Habitaciones> Listar()
+        {
+            return obj_habitaciones.Listar();
+        }
     }
 }
