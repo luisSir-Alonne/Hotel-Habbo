0633a713825386df983b3292d1d8e6a0cababfb2
Luis Ponce <luisponceprieto@gmail.com>
2025-11-10 22:20:16 -0600

    Septimo commit para la clase Rol en todas sus capas

--- Capa Negocio/CN_Rol.cs
+++ Capa Negocio/CN_Rol.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Capa_Datos;
+using Capa_Entidad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace Capa_Negocio
 {
     public class CN_Rol
     {
+        private CD_Rol obj_rol = new CD_Rol();
+        public List<Rol> Leer()
+        {
+            return obj_rol.Leer();
+        }
     }
 }
