7dd28782128c67d1a2992f56a664c9c4b2ed0c18
JAVIER24 Hdz <javieralexhv@gmail.com>
2025-11-12 13:58:46 -0600

    Avance checkout

--- MAP2A1HotelHeavens/CheckOut.cs
+++ MAP2A1HotelHeavens/CheckOut.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using CapaEntidad;
+using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +15,7 @@ namespace MAP2A1HotelHeavens
 {
     public partial class CheckOut : Form
     {
+
         
         public CheckOut()
         {
@@ -20,8 +24,18 @@ namespace MAP2A1HotelHeavens
 
         private void btnConfimar_Click(object sender, EventArgs e)
         {
-            string Huesped, Habitacíon, Telefono;
-            
+            List<Hotel> hotel = new List<Hotel>();
+            Hotel objusuario = new Hotel();
+            string mensaje = string.Empty;
+            bool respuesta = new CN_Hotel().Registrar(objusuario,out mensaje);
+           hotel = new CN_Hotel().Listar();
+
+            if (!respuesta)
+            {
+                MessageBox.Show(mensaje);
+
+            }
+
         }
 
         private void CheckOut_Load(object sender, EventArgs e)
@@ -37,7 +51,7 @@ namespace MAP2A1HotelHeavens
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
           
-            /* ji ji ji ja ListViewItem item1 = new ListViewItem(nombre);*/
+            
         }
     }
 }
