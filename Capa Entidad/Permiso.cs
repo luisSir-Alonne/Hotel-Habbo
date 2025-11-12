da80d6357b2c22465590cd90daa115911a787486
Luis Ponce <luisponceprieto@gmail.com>
2025-11-12 06:30:11 -0600

    Decimo commit para la nueva tabla de permisos con cada uno de los elementos

---
+++ Capa Entidad/Permiso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class Permiso
+    {
+        public int idPermiso { get; set; }
+        public int IdRol { get; set; }
+        public string descripcion { get; set; }
+
+    }
+}
