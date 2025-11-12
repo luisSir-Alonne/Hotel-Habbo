75ba9f327dbcf1b9a980dc69d72f58a662b131af
JAVIER24 Hdz <javieralexhv@gmail.com>
2025-11-12 11:40:37 -0600

    Editar diseño de checkout

--- MAP2A1HotelHeavens/CheckOut.cs
+++ MAP2A1HotelHeavens/CheckOut.cs
@@ -19,10 +19,17 @@ namespace MAP2A1HotelHeavens
 
         private void btnConfimar_Click(object sender, EventArgs e)
         {
-            
+            string Huesped, Habitacíon, Telefono;
+            Huesped = Convert.ToString(txtNombre.Text);
+            Habitacíon = Convert.ToString
         }
 
         private void CheckOut_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
