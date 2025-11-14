0c872796465d9737b676c7bd198b35731829ac4a
Luis Ponce <luisponceprieto@gmail.com>
2025-11-14 13:14:59 -0600

    Cambios implementados en el login

--- MAP2A1HotelHeavens/LogIn.cs
+++ MAP2A1HotelHeavens/LogIn.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaEntidad;
+using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +22,48 @@ namespace MAP2A1HotelHeavens
         private void LogIn_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void buttonIniciar_Click(object sender, EventArgs e)
+        {
+            List<Empleado> listaUsuarios = new CN_Empleado().Listar();
+            Empleado ousuario = new CN_Empleado().Listar().Where(u => u.nombre == textBoxUsuario.Text && u.clave == textBoxContraseña.Text).FirstOrDefault();
+            if (ousuario != null)
+            {
+                var n = new Form1();
+                n.Show();
+                this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("No se encontro al usuario");
+                return;
+            }
+        }
+
+        private void checkBoxOcultar1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxOcultar1.Checked)
+            {
+                textBoxUsuario.PasswordChar = '*';
+            }
+            else
+            {
+                textBoxUsuario.PasswordChar = ' ';
+            }
+        }
+
+        private void checkBoxOcultar2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxOcultar2.Checked)
+            {
+                textBoxContraseña.PasswordChar = '*';
+            }
+            else
+            {
+                
+            }
         }
     }
 }
